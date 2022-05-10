2a4be57bc4318f0f5ab0b64baa35bfdd6976ff74
bhuvidh-2001 <bhushan.dhumne19@gmail.com>
2022-05-10 22:00:40 +0530

    Added index, header and  footer.

---
+++ routes/index.js
@@ -0,0 +1,76 @@
+const route = require('express').Router()
+const User = require('../models/user')
+const CompanyUser = require('../models/companyuser')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+route.get('/register', (req, res) => {
+    res.render('register',{title: 'Register'})
+})
+
+route.post('/register', (req, res) => {
+    User.register(new User({
+        username: req.body.email,   
+        id :req.body.id
+    }), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err)
+            return res.render('register')
+        } else {
+            User.authenticate('local')(req, res, () => res.redirect('/'))
+        }
+
+    })
+})
+
+route.get('/companyregister',(req,res)=>{
+    res.render('companyregister',{title: 'Company Register'})
+})
+
+route.post('/companyregister', (req, res) => {
+
+    CompanyUser.register(new CompanyUser({
+        username: req.body.email,
+        Cid: req.body.Cid
+    }), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err)
+            return res.render('companyregister')
+        } else {
+            CompanyUser.authenticate('local')(req, res, () => res.redirect('/'))
+        }
+
+    })
+})
+
+route.get('/login', (req, res) => {
+    res.render('login', {title: 'login'})
+})
+route.post('/login', passport.authenticate('bloglocal', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}), (req, res) => {console.log(req.body.id + req.body.email +req.body.password)})
+
+route.get('/companylogin', (req, res) => {
+    res.render('companylogin',{title: 'Company login'})
+})
+
+route.post('/companylogin', passport.authenticate('companylocal', {
+    successRedirect: '/company',
+    failureRedirect: '/companylogin'
+}), (req, res) => {
+    console.log(req.body.id + req.body.email + req.body.password)
+})
+
+
+route.get('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/')
+})
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next()
+    res.redirect('/login')
+}
+
+module.exports = route
